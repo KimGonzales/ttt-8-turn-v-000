8b88263d257f849a288f5902de2dd63a5f5ac9de
Kim Gonzales <kimgo589@gmail.com>
2018-01-04 05:54:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,7 @@ end
 def valid_move?(board, index)
   board[index].between?(0,8) && !postion_taken?(board, index)
 end
+
+def position_taken? (board, index)
+  board[index]=="X" || board[index] == "O"
+end
